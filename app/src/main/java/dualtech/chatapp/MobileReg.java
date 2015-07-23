5b5d7b59a7a527c3fd762e75fe29391ef83e655d
tundetunde <tundetunde000@gmail.com>
2015-07-23 21:33:36 +0100

    Registration now works

--- app/src/main/java/dualtech/chatapp/MobileReg.java
+++ app/src/main/java/dualtech/chatapp/MobileReg.java
@@ -33,21 +33,13 @@ public class MobileReg extends Activity implements View.OnClickListener {
     Button reg_next;
     static String regId;
     static String phnNo;
-    String msg;
-    ProgressBar spinner;
-    GoogleCloudMessaging gcm;
     static SharedPreferences prefs;
-    private BroadcastReceiver mRegistrationBroadcastReceiver;
-    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     private static final String TAG = "Note"/*MainActivity*/;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         prefs = getSharedPreferences(ApplicationInit.SHARED_PREF, Context.MODE_PRIVATE);
         setContentView(R.layout.registration);
-
-        spinner=(ProgressBar)findViewById(R.id.progressBar);
-        spinner.setVisibility(View.GONE);
 
         mobileNum = (EditText) findViewById(R.id.phone_num);
         mobileNum.setText(VerifyNumber.getNO());
@@ -60,60 +52,19 @@ public class MobileReg extends Activity implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.bt_reg:
                 phnNo = ApplicationInit.getMobile_number();
-                spinner.setVisibility(View.VISIBLE);
-//                ApplicationInit.setMobile_number(phnNo);
+                BroadcastActivity.phnNo = phnNo;
+                BroadcastActivity.regId = regId;
+                Intent i = new Intent("dualtech.chatapp.BROADCASTACTIVITY");
+                startActivity(i);
 
-                mRegistrationBroadcastReceiver = new BroadcastReceiver() {
-                    @Override
-                    public void onReceive(Context context, Intent intent) {
-                        Log.d(TAG, "Entered onReceive");
-                        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-                        boolean sentToken = sharedPreferences.getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
-                        Log.d(TAG, String.valueOf(sentToken));
-                        if (sentToken) {
-                            Toast.makeText(MobileReg.this,"Activated",Toast.LENGTH_LONG).show();
-                        } else {
-                            Toast.makeText(MobileReg.this,"Cannot activation -- Try again",Toast.LENGTH_LONG).show();
-                        }
-                        MobileReg.storePref();
-                        Intent openMain = new Intent(context, MainActivity.class);
-                        openMain.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        openMain.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP );
-                        context.startActivity(openMain);
-
-                        spinner.setVisibility(View.GONE);
-                    }
-                };
-
-                LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
-                        new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+                /*LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                        new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));*/
 /*
                 IntentFilter filter = new IntentFilter();
                 filter.addAction("Reg_Receiver");
                 registerReceiver(mRegistrationBroadcastReceiver, filter);*/
-
-                if (checkPlayServices()) {
-                    // Start IntentService to register this application with GCM.
-                    Intent intent = new Intent(this, RegistrationIntentService.class);
-                    startService(intent);
-                }
         }
         }
-
-    private boolean checkPlayServices() {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
-                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            } else {
-                Log.i(TAG, "This device is not supported.");
-                finish();
-            }
-            return false;
-        }
-        return true;
-    }
 
     public static void storePref(){
         SharedPreferences.Editor editor = prefs.edit();
@@ -123,20 +74,6 @@ public class MobileReg extends Activity implements View.OnClickListener {
 
         ApplicationInit.setMobile_number(phnNo);
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
-    }
-
-    @Override
-    protected void onPause() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
-        super.onPause();
-    }
-
 
     private int msgId() {
         int id = prefs.getInt(ApplicationInit.KEY_MSG_ID, 0);
