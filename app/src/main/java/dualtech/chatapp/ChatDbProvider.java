1538fc42dad0ff7a722d4be5ddf1319599ad29db
Jessica <adajess01@gmail.com>
2015-09-01 06:02:16 +0100

    Update....

--- app/src/main/java/dualtech/chatapp/ChatDbProvider.java
+++ app/src/main/java/dualtech/chatapp/ChatDbProvider.java
@@ -3,13 +3,13 @@ package dualtech.chatapp;
 /**
  * Created by Jesz on 11-Aug-15.
  */
-public class TestChatDbProvider {
+public class ChatDbProvider {
     /** Chat history data provider
      * */
     String msg;
     int s_id;
     String date;
-    TestChatDbProvider(String s, int i, String d){
+    ChatDbProvider(String s, int i, String d){
         msg = s;
         s_id = i;
         date = d;
