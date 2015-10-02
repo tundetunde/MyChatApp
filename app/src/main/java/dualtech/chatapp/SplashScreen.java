1712e86b30bbdf1382fbe277d0e7cc1a56a14afd
Jessica <adajess01@gmail.com>
2015-10-02 22:24:58 +0100

    Receipt update

--- app/src/main/java/dualtech/chatapp/SplashScreen.java
+++ app/src/main/java/dualtech/chatapp/SplashScreen.java
@@ -30,14 +30,15 @@ public class SplashScreen extends Activity {
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }finally {
-                    Intent i = new Intent().setClass(getApplicationContext(), LoadContacts.class);
-                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startService(i);
                     Intent openMain = new Intent().setClass(getApplicationContext(), MainActivity.class);
                     openMain.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                 // If there is no registration ID, the app isn't registered.
                     if (ApplicationInit.getREGISTRATION_KEY()== null){
                         openMain = new Intent().setClass(getApplicationContext(), VerifyNumber.class);
+                    }else{
+                        Intent i = new Intent().setClass(getApplicationContext(), LoadContacts.class);
+                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startService(i);
                     }
                     openMain.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                     startActivity(openMain);
